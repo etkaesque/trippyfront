8c00170ce8aade0a1684cc908fa8d5dda3f5d4a9
etkaesque <edga.majo@gmail.com>
2023-07-24 12:01:35 +0300

    bug fix

--- src/pages/components/card/Card.tsx
+++ src/pages/components/card/Card.tsx
@@ -24,8 +24,13 @@ type CardProps = {
 export default function Card({ trip }: CardProps) {
 
 
-    let tripDate = trip.date.toString().split('T')[0]
-    const timeDifference = new Date().getTime() - new Date(trip.date_uploaded).getTime();
+   
+
+   
+    
+
+   
+    const timeDifference = new Date().getTime() - new Date(trip?.date_uploaded).getTime();
     const timeDifferenceInHours = timeDifference / (1000 * 60 * 60); 
 
 
@@ -33,8 +38,8 @@ export default function Card({ trip }: CardProps) {
 
   return (
     <>
-       
-      <div className="relative mx-auto w-full max-w-xs pt-6 h flex-col max-h-full">
+
+    { trip &&    <div className="relative mx-auto w-full max-w-xs pt-6 h flex-col max-h-full">
         <Link
           href={`trip/${trip.id}`}
           className="relative  w-full flex flex-col h-full transform transition-transform duration-300 ease-in-out flex-grow"
@@ -351,7 +356,7 @@ export default function Card({ trip }: CardProps) {
                   <svg  width="25px" height="25px" viewBox="0 0 24 24"><path d="m22 2.25h-3.25v-1.5c-.014-.404-.344-.726-.75-.726s-.736.322-.75.725v.001 1.5h-4.5v-1.5c-.014-.404-.344-.726-.75-.726s-.736.322-.75.725v.001 1.5h-4.5v-1.5c-.014-.404-.344-.726-.75-.726s-.736.322-.75.725v.001 1.5h-3.25c-1.104 0-2 .895-2 1.999v17.75c0 1.105.895 2 2 2h20c1.105 0 2-.895 2-2v-17.75c0-1.104-.896-1.999-2-1.999zm.5 19.75c0 .276-.224.499-.499.5h-20.001c-.276 0-.5-.224-.5-.5v-17.75c.001-.276.224-.499.5-.499h3.25v1.5c.014.404.344.726.75.726s.736-.322.75-.725v-.001-1.5h4.5v1.5c.014.404.344.726.75.726s.736-.322.75-.725v-.001-1.5h4.5v1.5c.014.404.344.726.75.726s.736-.322.75-.725v-.001-1.5h3.25c.276 0 .499.224.499.499z"/><path d="m5.25 9h3v2.25h-3z"/><path d="m5.25 12.75h3v2.25h-3z"/><path d="m5.25 16.5h3v2.25h-3z"/><path d="m10.5 16.5h3v2.25h-3z"/><path d="m10.5 12.75h3v2.25h-3z"/><path d="m10.5 9h3v2.25h-3z"/><path d="m15.75 16.5h3v2.25h-3z"/><path d="m15.75 12.75h3v2.25h-3z"/><path d="m15.75 9h3v2.25h-3z"/></svg>
                   <span className="mt-0">
                     {" "}
-                    {tripDate}
+                    {trip.date?.toString().split('T')[0]}
                   </span>
                 </div>
 
@@ -391,7 +396,9 @@ export default function Card({ trip }: CardProps) {
             </div>
           </div>
         </Link>
-      </div>
+      </div>}
+       
+   
     </>
   );
 }
